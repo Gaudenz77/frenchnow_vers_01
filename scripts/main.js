aa59402be691db9273404b5928e0d92875b68a3c
Gaudenz@Krakatoom <42372680+Gaudenz77@users.noreply.github.com>
2024-01-09 15:20:41 +0100

    hambureger and banner included

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,9 @@
 // Add your javascript here
+
+// HAMBURGER MENU SCRIPT ----------------------------------------------------------------
+const menuButton = document.querySelector("a#menu-icon");
+
+menuButton.addEventListener("click", function(e) {
+  e.preventDefault();
+  menuButton.classList.toggle("close");
+});
