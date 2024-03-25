0615afebf71dda351d844ba5bbe5628425d0242e
Gaudenz@Krakatoom <42372680+Gaudenz77@users.noreply.github.com>
2024-03-25 19:45:31 +0100

    working

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,46 @@
-// Call the function when the DOM content is loaded
-document.addEventListener("DOMContentLoaded", detectBrowserLanguageAndRedirect);
+// Function to get the user's browser language
+function getUserLanguage() {
+  return navigator.language || navigator.userLanguage;
+}
+
+// Variable to track whether redirection has been performed
+var redirectionPerformed = false;
+
+// Function to redirect the user based on their browser language
+function redirectToLanguagePage() {
+  // If redirection has already been performed, return
+  if (redirectionPerformed) {
+    return;
+  }
+
+  var userLanguage = getUserLanguage();
+
+  // Check if the current page is one of the language pages
+  var currentPage = window.location.pathname;
+  if (currentPage === '/index_french.html' || currentPage === '/index_rus.html' || currentPage === '/index.html') {
+    // Page is already one of the language pages, set the flag and return
+    redirectionPerformed = true;
+    return;
+  }
+
+  // Check the user's language and redirect accordingly
+  if (userLanguage.startsWith('fr')) {
+    window.location.href = 'index_french.html';
+  } else if (userLanguage.startsWith('ru')) {
+    window.location.href = 'index_rus.html';
+  } else {
+    // Default redirection (if the user's language is not supported)
+    window.location.href = 'index.html';
+  }
+
+  // Set the flag to indicate that redirection has been performed
+  redirectionPerformed = true;
+}
 
 // Call the redirection function when the page loads
-redirectToLanguagePage();
-// Call the function when the DOM content is loaded
-document.addEventListener("DOMContentLoaded", detectBrowserLanguageAndRedirect);
+document.addEventListener('DOMContentLoaded', redirectToLanguagePage);
+
+
 
 // HAMBURGER MENU SCRIPT ----------------------------------------------------------------
   const menuButton = document.querySelector("a#menu-icon");
